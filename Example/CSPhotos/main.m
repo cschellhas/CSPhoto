207b54280f7dffb7de0aca52ad91ec0f4b7b7426
Christoph Schellhas <christoph.schellhas@googlemail.com>
2017-02-28 10:27:34 +0100

    Initial commit

---
+++ Example/CSPhotos/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CSPhotos
+//
+//  Created by cschellhas on 02/28/2017.
+//  Copyright (c) 2017 cschellhas. All rights reserved.
+//
+
+@import UIKit;
+#import "cspAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([cspAppDelegate class]));
+    }
+}
